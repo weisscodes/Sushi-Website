95976f9a997ed981a4326f390973f62d20e55d0a
Ellathiel <98175801+weisscodes@users.noreply.github.com>
2024-04-06 22:46:46 +0200

    Installed AOS library

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-// import AOS from "aos";
-// import "aos/dist/aos.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
-// // init AOS animation
-// AOS.init({
-//   duration: 1000,
-//   offset: 100,
-// });
+// init AOS animation
+AOS.init({
+  duration: 1000,
+  offset: 100,
+});
 
-// // the additional code you saw in the video will not be needed :)
+// the additional code you saw in the video will not be needed :)
